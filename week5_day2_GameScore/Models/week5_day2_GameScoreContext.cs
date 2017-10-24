43cba767e793d736bb0c06eb085f1df1415ca7e2
PensCup87 <bdt9123@gmail.com>
2017-10-24 15:10:30 -0400

    Added sores to list

---
+++ week5_day2_GameScore/Models/week5_day2_GameScoreContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace week5_day2_GameScore.Models
+{
+    public class week5_day2_GameScoreContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public week5_day2_GameScoreContext() : base("name=week5_day2_GameScoreContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<week5_day2_GameScore.Models.Score> Scores { get; set; }
+    }
+}
